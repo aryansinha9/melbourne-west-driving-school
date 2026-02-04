e6829863e1bd16b5f042a288b2f164a15802c56b
Aryan Sinha <z5589770@ad.unsw.edu.au>
2026-02-04 13:21:40 +1100

    Add Head component for global metadata

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,16 @@
 import Layout from '@/components/Layout';
 import '@/styles/globals.css';
 
+import Head from 'next/head';
+
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <Head>
+        <title>Melbourne West Driving School</title>
+        <meta name="description" content="Safe and confident driving lessons in Melbourne West." />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
